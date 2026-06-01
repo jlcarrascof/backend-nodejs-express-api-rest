68389490db921081464cf0e0ae1942e04bff5320
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-05-31 22:38:16 -0400

    feat(routing): add new-route and products endpoints in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('My Express Server using JavaScript');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send('This is a new route - using JavaScript');
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+})
+
 app.listen(port, () => {
   console.log('App Node with JavaScript running at port ' + port);
 });
