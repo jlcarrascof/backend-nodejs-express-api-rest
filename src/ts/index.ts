8c890567f560f4755be89ff0d84e474db8a8fd36
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-05-31 21:18:12 -0400

    feat(express): implement first express server in typescript

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,12 @@
-console.log('My App en TypeScript');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('My Express Server using TypeScript');
+});
+
+app.listen(port, () => {
+  console.log(`App Node with TypeScript running at port ${port}`);
+});
