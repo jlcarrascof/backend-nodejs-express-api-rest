158937aec181cb4bdd72ff68b2eb8bc7b511b0f4
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-05-31 22:38:55 -0400

    feat(routing): add new-route and products endpoints in typescript

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,6 +7,17 @@ app.get('/', (req: Request, res: Response) => {
   res.send('My Express Server using TypeScript');
 });
 
+app.get('/new-route', (req: Request, res: Response) => {
+  res.send('This is a new route - using TypeScript');
+});
+
+app.get('/products', (req: Request, res: Response) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
 app.listen(port, () => {
   console.log(`App Node with TypeScript running at port ${port}`);
 });
