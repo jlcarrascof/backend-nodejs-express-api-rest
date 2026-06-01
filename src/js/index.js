3fcad5c85b85b4a918821fa81045d6d205175e18
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-05-31 21:17:33 -0400

    feat(express): first express server in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('My Express Server using JavaScript');
+});
+
+app.listen(port, () => {
+  console.log('App Node with JavaScript running at port ' + port);
+});
+
