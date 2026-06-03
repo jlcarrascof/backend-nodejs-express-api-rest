11e9eb95ffcc0532adfb4585f90277809a3de3c6
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-03 12:35:30 -0400

    feat(routing): add get product by id endpoint using params in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -40,6 +40,18 @@ app.get('/products', (req, res) => {
   ])
 })
 
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json(
+    {
+      id,
+      name: 'Product X',
+      price: 2000,
+    }
+  )
+})
+
 app.listen(port, () => {
   console.log('App Node with JavaScript running at port ' + port);
 });
