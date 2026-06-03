15d5b036c04bb4a63b038e2c9bbb2115cc24c684
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-03 12:58:30 -0400

    feat(routing): add categories and products complex params route in typescript

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -12,9 +12,29 @@ app.get('/new-route', (req: Request, res: Response) => {
 });
 
 app.get('/products', (req: Request, res: Response) => {
+  res.json([
+    { id: 1, name: 'Product 1', price: 1000 },
+    { id: 2, name: 'Product 2', price: 2000 },
+    { id: 3, name: 'Product 3', price: 3000 },
+    { id: 4, name: 'Product 4', price: 4000 },
+    { id: 5, name: 'Product 5', price: 5000 }
+  ]);
+});
+
+app.get('/products/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000
+    id,
+    name: 'Product X',
+    price: 2000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req: Request, res: Response) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
