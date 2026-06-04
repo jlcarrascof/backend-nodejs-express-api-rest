c6c1cf373a1a11a54aa606b228b7c2e84cf16d9a
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-04 16:49:14 -0400

    feat(routing): extract products routes into separate router file in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -12,37 +12,6 @@ app.get('/new-route', (req, res) => {
   res.send('This is a new route - using JavaScript');
 })
 
-app.get('/products', (req, res) => {
-
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send(`I'm a filter`);
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-
-  res.json(
-    {
-      id,
-      name: 'Product X',
-      price: 2000,
-    }
-  )
-});
-
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
 
