ea6e1dde5d7ddcf7521fb64dc4093e0ae1a6a9b0
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-04 18:19:27 -0400

    feat(routing): complete routes modularization and move into src/js in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
-
+const routerApi = require('./routes')
 const app = express();
 const port = 3000;
 
@@ -12,31 +11,8 @@ app.get('/new-route', (req, res) => {
   res.send('This is a new route - using JavaScript');
 })
 
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-
-  res.json({
-    categoryId,
-    productId,
-  });
-});
-
-// Query params
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    })
-  } else {
-    res.send(`There aren't parameters`);
-  }
-})
-
 app.listen(port, () => {
   console.log('App Node with JavaScript running at port ' + port);
 });
 
+routerApi(app);
