ea8ca5fa66ec4b12c7417f49b4f75dc452b0a197
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-03 21:46:31 -0400

    feat(mocking): apply size query param to limit random products generation in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -15,8 +15,9 @@ app.get('/new-route', (req, res) => {
 app.get('/products', (req, res) => {
 
   const products = [];
-
-  for (let index = 0; index < 100; index++) {
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
     products.push({
       name: faker.commerce.productName(),
       price: parseInt(faker.commerce.price(), 10),
