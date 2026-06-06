0a7f1d0765200b48298538f60fdd543e4f3bfb3b
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-06 12:16:12 -0400

    feat(middleware): add express.json middleware and fix routerApi call order in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const routerApi = require('./routes')
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('My Express Server using JavaScript');
@@ -11,8 +14,9 @@ app.get('/new-route', (req, res) => {
   res.send('This is a new route - using JavaScript');
 })
 
+routerApi(app);
+
 app.listen(port, () => {
   console.log('App Node with JavaScript running at port ' + port);
 });
 
-routerApi(app);
