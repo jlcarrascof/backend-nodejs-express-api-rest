427b52d5591f396ef246d94497cbf8e4f7f3249e
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-06 21:32:02 -0400

    feat(products): add http status codes to endpoints in javascript

--- src/js/routes/products.router.js
+++ src/js/routes/products.router.js
@@ -24,19 +24,22 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-
-  res.json(
-    {
+  if (id === '999') {
+    res.status(404).json({
+      message: 'Not Found'
+    });
+  } else {
+    res.status(200).json({
       id,
       name: 'Product X',
       price: 2000,
-    }
-  )
+    });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   });
