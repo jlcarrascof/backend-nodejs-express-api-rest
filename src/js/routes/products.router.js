8449fe37e21b11726a44f716f05151227ea00799
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-12 23:11:32 -0400

    feat(products): move mock data generation logic to ProductsService

--- src/js/routes/products.router.js
+++ src/js/routes/products.router.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
 
 const router = express.Router();
 
@@ -7,14 +6,7 @@ router.get('/', (req, res) => {
 
   const products = [];
   const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+
   res.json(products);
 });
 
