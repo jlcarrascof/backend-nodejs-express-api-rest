a2e4b197e55c2f9b6ee6c443ebd3dec45a20398f
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-19 13:55:28 -0400

    feat(products): connect create route to products service

--- src/js/routes/products.router.js
+++ src/js/routes/products.router.js
@@ -25,10 +25,8 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body
-  });
+  const newProduct = service.create(body);
+  res.status(201).json(newProduct);
 })
 
 router.patch('/:id', (req, res) => {
