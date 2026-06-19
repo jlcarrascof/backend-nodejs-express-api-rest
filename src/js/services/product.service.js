342ba9bf747c8045ea0f2abc4c757c5b71a10f26
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-19 12:42:38 -0400

    feat(products): implement create method in products service

--- src/js/services/product.service.js
+++ src/js/services/product.service.js
@@ -19,8 +19,13 @@ class ProductsService {
     }
   }
 
-  create() {
-
+  create(data) {
+    const newProduct = {
+      id: faker.string.uuid(),
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   find() {
