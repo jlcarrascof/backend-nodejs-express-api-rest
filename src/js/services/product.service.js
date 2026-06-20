f5d98dead029266ac5890ea424c8e7b717215676
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-19 22:04:11 -0400

    feat(products): implement delete method in products service

--- src/js/services/product.service.js
+++ src/js/services/product.service.js
@@ -45,8 +45,13 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete() {
-
+  delete(id) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
   }
 
 }
