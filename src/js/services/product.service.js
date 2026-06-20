c00ef7acb11daf0839b20bf81da485a71a0f1583
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-19 21:16:10 -0400

    feat(products): implement update method in products service

--- src/js/services/product.service.js
+++ src/js/services/product.service.js
@@ -36,8 +36,13 @@ class ProductsService {
     return this.products.find(item => item.id === id);
   }
 
-  update() {
-
+  update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products[index] = changes;
+    return this.products[index];
   }
 
   delete() {
