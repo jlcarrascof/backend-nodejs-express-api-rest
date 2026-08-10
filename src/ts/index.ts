924c2c88426740918084969cb75b51bbe2e3d626
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-09 22:55:45 -0400

    feat(middlewares): integrate typescript error handlers in main express app

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import routerApi from './routes';
+import { logErrors, errorHandler } from './middlewares/error.handler';
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,9 @@ app.get('/new-route', (req: Request, res: Response) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App Node with TypeScript running at port ${port}`);
--- src/ts/middlewares/error.handler.ts
+++ src/ts/middlewares/error.handler.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function logErrors(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.log('logErrors');
   console.error(err);
   next(err);
 }
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.log('errorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack,
