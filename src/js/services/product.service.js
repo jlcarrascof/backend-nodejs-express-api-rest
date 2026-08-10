7f02652de5e7a964b32b3eba98efdc57d72151d5
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-10 12:25:55 -0400

    feat(products): integrate boom for advanced error handling in service

--- src/js/services/product.service.js
+++ src/js/services/product.service.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -37,14 +38,17 @@ class ProductsService {
   }
 
   async findOne(id) {
-    const name = this.getTotal();
-    return this.products.find(item => item.id === id);
+    const product = this.products.find(item => item.id === id);
+    if (!product) {
+      throw boom.notFound('Product Not Found!');
+    }
+    return product;
   }
 
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error('Product not found');
+      throw boom.notFound('Product Not Found!');
     }
     const product = this.products[index];
     this.products[index] = {
@@ -57,7 +61,7 @@ class ProductsService {
   async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error('Product not found');
+      throw boom.notFound('Product Not Found!');
     }
     this.products.splice(index, 1);
     return { id };
