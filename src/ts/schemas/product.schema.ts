3e01e765cc6735da865a861d43cfb3900528e221
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-10 20:51:24 -0400

    feat(schemas): add update and get product schemas in typescript

--- src/ts/schemas/product.schema.ts
+++ src/ts/schemas/product.schema.ts
@@ -8,3 +8,12 @@ export const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
 });
+
+export const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+});
+
+export const getProductSchema = Joi.object({
+  id: id.required(),
+});
