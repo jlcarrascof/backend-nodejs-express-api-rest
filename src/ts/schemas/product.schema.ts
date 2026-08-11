5a8accac01649d03a699c34202b553e03c0cc24d
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-10 20:38:20 -0400

    feat(schemas): initialize joi validation schemas for products in typescript

---
+++ src/ts/schemas/product.schema.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(15);
+const price = Joi.number().integer().min(10);
+
+export const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required(),
+});
